9568b8a4992e48493e93dd9d19ca6231ac5cfc7c
rusokverse <rusokverse@MACBOOKPRO>
2022-10-11 18:59:56 -0400

    Creando herramientas

---
+++ ASPNET3/Herramientas/Conversor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace ASPNET3.Herramientas
+{
+    public static class Conversor
+    {
+        public static void GuardarObjeto(
+            this ISession sesion, string llave, object valor) {
+            sesion.SetString(llave,JsonConvert.SerializeObject(valor));
+        }
+        public static T RecuperarObjeto<T>(
+            this ISession sesion, string llave) {
+            var valor = sesion.GetString(llave);
+            return valor == null ? default(T)
+                : JsonConvert.DeserializeObject<T>(valor);
+        }
+        // Persona pp = Conversor.RecuperarObjeto<Persona>("per");
+    }
+}
