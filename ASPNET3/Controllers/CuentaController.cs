16ee8ed45cb6f00529aaa842ee6b2b1262acd9ae
rusokverse <rusokverse@MACBOOKPRO>
2022-10-11 19:49:55 -0400

    Ejemplos de sesión.

---
+++ ASPNET3/Controllers/CuentaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET3.Controllers
+{
+    public class CuentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();//Crear vista... formulario de login
+        }
+
+        [HttpPost]
+        public IActionResult login(string usuario, string password) {
+            if (usuario != null && password != null && usuario.Equals("admin")
+                && password.Equals("admin"))
+            {
+                HttpContext.Session.SetString("usuario", usuario);
+                return View("Exito");
+            }
+            else {
+                ViewBag.mensaje = "No existe la cuenta o el password fue mal escrito";
+                return View("Index");
+            }
+        }
+
+        //Método para logout
+        [HttpGet]
+        public IActionResult logout() {
+            HttpContext.Session.Remove("usuario");
+            return RedirectToAction("Index");//Le avisa al cliente
+        }
+    }
+}
---
+++ ASPNET3/Controllers/PruebaSesionController.cs
@@ -0,0 +1,48 @@
+﻿using ASPNET3.Herramientas;
+using ASPNET3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASPNET3.Controllers
+{
+    public class PruebaSesionController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Guardando primitivas en la sesión
+            HttpContext.Session.SetInt32("cantidad",12);
+            HttpContext.Session.SetString("usuario","pepe123");
+
+            //Guardando un objeto en la sesión
+            Producto p = new Producto { 
+                id = 1,
+                Nombre = "Atún",
+                precio = 10.5
+            };
+            Conversor.GuardarObjeto(HttpContext.Session, "prod", p);
+
+            //Guardando una lista de objetos en la sesión
+            List<Producto> productos = new List<Producto>() {
+                new Producto {
+                id = 11,
+                Nombre = "Queso",
+                precio = 30
+                },
+                new Producto {
+                id = 12,
+                Nombre = "Mantequilla",
+                precio = 12
+                },
+                new Producto {
+                id = 13,
+                Nombre = "Gaseosa 3L",
+                precio = 10.5
+                }
+            };
+            Conversor.GuardarObjeto(HttpContext.Session, "productos", productos);
+
+            return View("Index");
+        }
+    }
+}
